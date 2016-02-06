5eba2008bdd1d6c67ffc3df380d811c14fdb37e8
CKiilu <cndeti@gmail.com>
2016-02-06 11:16:43 +0300

    first attempt at DOM manipulation

---
+++ Dommanipulation.js
@@ -0,0 +1,5 @@
+document.querySelector('p').textContent = "I'm on the page!"
+document.querySelector('h1').textContent = "Behold! For I am a mighty web developer!";
+var big_shot = document.querySelector('#primary');
+big_shot.textContent = "Flippin' heck I'm by far the most important element around."
+document.querySelector('.important').textContent = "Do all .important elements change or only one?"
