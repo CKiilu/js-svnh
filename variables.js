c20a17b2939e42f8de03862d8a96d7bfd1367080
CKiilu <cndeti@gmail.com>
2016-02-06 10:44:40 +0300

    Multiple var, use string in console

--- variables.js
+++ variables.js
@@ -14,3 +14,11 @@ var hour, minute, midnighth, midnightm;
 hour = 10, minute = 30, midnighth = 23, midnightm = 60;
 var time = (((midnighth - hour) * 60) + (midnightm - minute)) * 60;
 window.console.log(time);
+
+//Var create en masse
+var name = "Jake",
+    age = 20,
+    birthday = "December 25, 2005",
+    hometown, occupation = "coder",
+    food, friend, height, weight, tree;
+console.log("I am %s and I am  a %d year old %s. My birthday is on %s", name, age, occupation, birthday)
