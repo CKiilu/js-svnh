cb83e3ff9922835b8ec26f16dd4e394215e15384
CKiilu <cndeti@gmail.com>
2016-02-06 14:18:58 +0300

    LOTR path js

---
+++ scripts/path.js
@@ -0,0 +1,13 @@
+var response, image_path;
+var secret_saying = document.querySelector('#message').textContent;
+
+if (secret_saying === "You jest") {
+    response = "Open Sesame!!";
+    image_path = "images/open_door.png";
+} else {
+    response = "You shall not pass!!";
+    image_path = "images/gandalf.png";
+}
+
+document.querySelector('#result').textContent = response;
+document.querySelector('#img').textContent = image_path;
