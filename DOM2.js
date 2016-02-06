2cc97c3cd1e15546517175b271e8694eb58db116
CKiilu <cndeti@gmail.com>
2016-02-06 11:28:12 +0300

    Using prompt and queryselec to add data while page is live

---
+++ DOM2.js
@@ -0,0 +1,5 @@
+var visitor_name = window.prompt("Kindly enter your name.");
+var welcome_message = "Hello" + visitor_name;
+document.querySelector('#greeting').textContent = welcome_message;
+var town = window.prompt("What is your hometown?");
+document.querySelector('#hometown').textContent = town;
