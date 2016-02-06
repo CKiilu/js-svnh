f2e3e68e6b911c6a1384f355c57b49d2e4fe9468
CKiilu <cndeti@gmail.com>
2016-02-06 14:09:24 +0300

    hero js

---
+++ scripts/hero.js
@@ -0,0 +1,8 @@
+var hero, bad_guy;
+
+if (hero === "strong") {
+    if (bad_guy === "weak") {
+        console.log('Ah-ha, an easy victory!');
+    }
+    console.log('Let us battle to the death!');
+}
