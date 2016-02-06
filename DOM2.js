76554d55acf429f4104b167d86e5b39e0268ca17
CKiilu <cndeti@gmail.com>
2016-02-06 12:28:06 +0300

    Mad libs, par by js

--- DOM2.js
+++ DOM2.js
@@ -1,6 +1,6 @@
 //User input on web page
 var visitor_name = window.prompt("Kindly enter your name.");
-var welcome_message = "Hello" + visitor_name;
+var welcome_message = "Hello " + visitor_name;
 document.querySelector('#greeting').textContent = welcome_message;
 var town = window.prompt("What is your hometown?");
 document.querySelector('#hometown').textContent = town;
---
+++ Madlibs.js
@@ -0,0 +1,14 @@
+var word1 = document.querySelector('#pro1').textContent = "He ";
+var word2 = document.querySelector('#verb1').textContent = "ran ";
+var word3 = document.querySelector('#prep1').textContent = "away! ";
+var word4 = document.querySelector('#que1').textContent = "How ";
+var word5 = document.querySelector('#verb2').textContent = "could ";
+var word6 = document.querySelector('#pro2').textContent = "he? ";
+var word7 = document.querySelector('#att1').textContent = "The ";
+var word8 = document.querySelector('#noun1').textContent = "hero? ";
+var word9 = document.querySelector('#comp1').textContent = "More ";
+var word10 = document.querySelector('#comp2').textContent = "like ";
+var word11 = document.querySelector('#noun2').textContent = "coward ";
+var word12 = document.querySelector('#prep2').textContent = "of ";
+var word13 = document.querySelector('#att2').textContent = "the ";
+var word14 = document.querySelector('#noun3').textContent = "century!"
